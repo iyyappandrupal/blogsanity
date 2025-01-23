aa4621e25f5fb34c6f5a49fa65e57001bfd5e204
iyyappandrupal <iyyappan.govindasamy@axelerant.com>
2025-01-23 18:38:29 +0530

    Disable issue resolved.

--- src/app/api/draft-mode/disable/route.ts
+++ src/app/api/draft-mode/disable/route.ts
@@ -1,9 +1,10 @@
 // app/api/draft-mode/disable/route.ts
 
-import { draftMode } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
+import { draftMode } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  await draftMode().disable()
-  return NextResponse.redirect(new URL('/', request.url))
+  const draftModeInstance = draftMode(); // Get the draftMode instance
+  draftModeInstance.disable();          // Disable draft mode
+  return NextResponse.redirect(new URL('/', request.url));
 }

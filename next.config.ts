05028cbecc6a3e1f99a81b22cf86e398d30d24cb
iyyappandrupal <iyyappan.govindasamy@axelerant.com>
2025-01-24 13:11:55 +0530

    Added the netlify file

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,14 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const NextConfig = {
   images: {
-    domains: ["cdn.sanity.io", "via.placeholder.com"],
     remotePatterns: [
       {
         protocol: "https", // or "http"
         hostname: "cdn.sanity.io", // Replace with the domain of the image source
-        port: "", // Leave empty unless a specific port is required
-        pathname: "/**", // Use ** for wildcard matching
       },
       {
         protocol: "https", // or "http"
         hostname: "via.placeholder.com", // Replace with the domain of the image source
-        port: "", // Leave empty unless a specific port is required
-        pathname: "/550x310", // Use ** for wildcard matching
       },
     ],
   },
